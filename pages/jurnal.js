f19d8b771e9a92db9237f07ec943b6ed03292bf5
danu999 <danusaputraoffice@gmail.com>
2023-03-29 09:04:53 +0700

    Update

--- pages/jurnal.js
+++ pages/jurnal.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Table, Popconfirm, Button } from "antd";
+import { Table, Popconfirm, Button, Space, Form, Input } from "antd";
 import { isEmpty } from "lodash";
 import axios from "axios";
 import Layout from "@/layout/Layout";
@@ -8,6 +8,8 @@ import styles from "@/styles/Jurnal.module.css";
 const Jurnal = () => {
   const [gridData, setGridData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [editRowKey, setEditRowKey] = useState("");
+  const [form] = Form.useForm();
 
   useEffect(() => {
     loadData();
@@ -37,6 +39,39 @@ const Jurnal = () => {
     const dataSource = [...modifiedData];
     const filteredData = dataSource.filter(item => item.id !== value.id);
     setGridData(filteredData);
+  };
+
+  const isEditing = record => {
+    return record.key === editRowKey;
+  };
+
+  const cancel = () => {
+    setEditRowKey("");
+  };
+  const save = async key => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...modifiedData];
+      const index = newData.findIndex(item => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, { ...item, ...row });
+        setGridData(newData);
+        setEditRowKey("");
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const edit = record => {
+    form.setFieldsValue({
+      name: "",
+      email: "",
+      message: "",
+      ...record,
+    });
+    setEditRowKey(record.key);
   };
 
   const columns = [
@@ -72,35 +107,114 @@ const Jurnal = () => {
       title: "Action",
       dataIndex: "action",
       align: "center",
-      render: (_, record) =>
-        modifiedData.length >= 1 ? (
-          <Popconfirm
-            title='Are you sure want to delete ?'
-            onConfirm={() => handleDelete(record)}
-          >
-            <Button danger type='primary'>
-              Delete
-            </Button>
-          </Popconfirm>
-        ) : null,
+      render: (_, record) => {
+        const editable = isEditing(record);
+        return modifiedData.length >= 1 ? (
+          <Space>
+            <Popconfirm
+              title='Are you sure want to delete ?'
+              onConfirm={() => handleDelete(record)}
+            >
+              <Button danger type='primary' disabled={editable}>
+                Delete
+              </Button>
+            </Popconfirm>
+            {editable ? (
+              <span>
+                <Space size='middle'>
+                  <Button
+                    onClick={e => save(record.key)}
+                    type='primary'
+                    style={{ marginRight: 8 }}
+                  >
+                    Save
+                  </Button>
+                  <Popconfirm title='Are sure to cancel ?' onConfirm={cancel}>
+                    <Button>Cancel</Button>
+                  </Popconfirm>
+                </Space>
+              </span>
+            ) : (
+              <Button onClick={() => edit(record)} type='primary'>
+                Edit
+              </Button>
+            )}
+          </Space>
+        ) : null;
+      },
     },
   ];
+
+  const mergedColumns = columns.map(col => {
+    if (!col.editTable) {
+      return col;
+    }
+
+    return {
+      ...col,
+      onCell: record => ({
+        record,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
+
+  const EditableCell = ({
+    editing,
+    dataIndex,
+    title,
+    record,
+    children,
+    ...restProps
+  }) => {
+    const input = <Input />;
+
+    return (
+      <td {...restProps}>
+        {editing ? (
+          <Form.Item
+            name={dataIndex}
+            style={{ margin: 0 }}
+            rules={[
+              {
+                required: true,
+                message: `Please input some value in ${title} field`,
+              },
+            ]}
+          >
+            {input}
+          </Form.Item>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  };
 
   return (
     <Layout>
       <div className={styles.header}>
         <h1>Jurnal</h1>
-        <Table
-          columns={columns}
-          dataSource={modifiedData}
-          bordered
-          loading={loading}
-          style={{
-            width: "90rem",
-            backgroundColor: "#ffcf00",
-            marginTop: "4rem",
-          }}
-        />
+        <Form from={form} component={false}>
+          <Table
+            columns={mergedColumns}
+            components={{
+              body: {
+                cell: EditableCell,
+              },
+            }}
+            dataSource={modifiedData}
+            bordered
+            loading={loading}
+            style={{
+              width: "90rem",
+              backgroundColor: "#ffcf00",
+              marginTop: "4rem",
+            }}
+          />
+        </Form>
       </div>
     </Layout>
   );
