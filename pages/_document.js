df56717eb9876c9a6965e210553b356ab5d9c3ae
danu999 <danusaputraoffice@gmail.com>
2023-04-11 11:12:39 +0700

    Update-Laporan-Again

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head />
+      <link rel='icon' type='image/svg+xml' href='/logo.svg' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <title>Migas Accounting</title>
       <body>
         <Main />
         <NextScript />
