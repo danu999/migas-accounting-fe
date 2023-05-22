03ae7d644ff16f226a25ca07b494bebd4a2d821a
danu999 <danusaputraoffice@gmail.com>
2023-05-22 14:43:11 +0700

    update login

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,7 @@
 import styles from "@/styles/Login.module.css";
-import { Button, Form, Input, Card } from "antd";
-import bg from "@/public/bg.jpg";
+import { Button, Form, Input, Card, Typography } from "antd";
+import Image from "next/image";
+import bg from "@/public/bg2.jpg";
 const onFinish = values => {
   console.log("Success:", values);
 };
@@ -13,16 +14,59 @@ const Login = () => {
     <div
       className={styles.login}
       style={{
-        backgroundImage: `url(${bg.src})`,
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${bg.src})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
       <Card
         className={styles.card}
-        bodyStyle={{ display: "flex", justifyContent: "center" }}
+        bodyStyle={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
       >
+        <Image
+          src='/logo.png'
+          width={250}
+          height={250}
+          alt='Logo Picture'
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginLeft: "5.5rem",
+          }}
+        ></Image>
+        <Typography
+          className={styles.typography}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "1rem",
+            fontSize: "20px",
+          }}
+        >
+          Sistem Akuntansi
+        </Typography>
+        <Typography
+          className={styles.typography}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "20px",
+          }}
+        >
+          Koperasi Primadaya Migas
+        </Typography>
         <Form
+          className={styles.form}
           name='basic'
           labelCol={{
             span: 8,
