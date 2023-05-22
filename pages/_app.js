2f0fc2e6e66f0d247f89edaff6948f730c278f17
danu999 <danusaputraoffice@gmail.com>
2023-05-22 13:19:17 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <SidebarProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </SidebarProvider>
   );
 }
--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,82 @@
+import styles from "@/styles/Login.module.css";
+import { Button, Form, Input, Card } from "antd";
+import bg from "@/public/bg.jpg";
+const onFinish = values => {
+  console.log("Success:", values);
+};
+const onFinishFailed = errorInfo => {
+  console.log("Failed:", errorInfo);
+};
+
 const Login = () => {
   return (
-    <div>
-      <h1>Welcome to Login page</h1>
+    <div
+      className={styles.login}
+      style={{
+        backgroundImage: `url(${bg.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Card
+        className={styles.card}
+        bodyStyle={{ display: "flex", justifyContent: "center" }}
+      >
+        <Form
+          name='basic'
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 20,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete='off'
+        >
+          <Form.Item
+            label='Username'
+            name='username'
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label='Password'
+            name='password'
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            wrapperCol={{
+              offset: 9,
+              span: 16,
+            }}
+          >
+            <Button type='primary' htmlType='submit'>
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   );
 };
